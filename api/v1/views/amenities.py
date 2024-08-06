42b21fcc9339a4172d126c3d1409735414ad63ad
adisu-mulu <kulhabesh31@gmail.com>
2024-08-06 22:10:49 +0300

    amenitites

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -19,7 +19,7 @@ def get_amenities():
     return jsonify(list_amenities)
 
 
-@app_views.route('/amenities/<amenity_id>/', methods=['GET'],
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
                  strict_slashes=False)
 def get_amenity(amenity_id):
     """ Retrieves an amenity """
