409413ef98d7f247daa5281364d03f0a62834fec
adisu-mulu <kulhabesh31@gmail.com>
2024-08-06 10:55:41 +0300

    retrieve number of each objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 """This module defines the status route"""
-
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """returning status on status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def num_of_each_objects():
+    """ retrieve number of each objects"""
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objects = {}
+    for i in range(len(classes)):
+        num_objects[names[i]] = storage.count(classes[i])
+    return jsonify(num_objects)
